0bb7928988a8746dc57daa0cb489600c937cbc47
Enda Lee <enda.lee@ittdublin.ie>
2016-12-15 17:02:27 +0000

    finished

--- src/app/bmi-calculator/bmi-calculator.component.ts
+++ src/app/bmi-calculator/bmi-calculator.component.ts
@@ -16,111 +16,66 @@ export class BmiCalculatorComponent implements OnInit {
   feet : number = 0;
   inches : number = 0;
   meters : number = 0;
-
+  value : number = 0;
   bmi : number = 0;
+  bmiString : string;
   counter : number = 0;
 
-  poundToKilo : number = 0; 
-  stoneToKilo : number = 0; 
-  poundToStone : number = 0; 
-  kiloToStone : number = 0; 
-  stoneToPound : number = 0; 
-  kiloToPound : number = 0; 
+KiloToPound(){
+  this.value = this.kilos / 0.453592;
+  this.pounds = this.value %14;
+  this.stone = (this.value - this.pounds) / 14;
+  this.pounds = Math.round(this.pounds);
+}
 
-  feetToMetre : number = 0; 
-  inchToMetre : number = 0; 
-  feetToInch : number = 0; 
-  meterToInch : number = 0; 
-  inchToFeet : number = 0; 
-  metreToFeet : number = 0; 
+PoundToKilo(){
   
-  BMI() {
-    
-     this.poundToKilo = this.pounds * (1 /2.2);
-     this.stoneToKilo = this.stone / 6.35029318;
-     this.poundToStone = this.pounds * 0.071429;
-     this.kiloToStone = this.kilos * 0.15747;
-     this.stoneToPound = this.stone * 14;
-     this.kiloToPound = this.kilos * 2.2046;
+  this.value = (this.pounds + (this.stone * 14));
+  this.kilos = this.value * 0.453592;
+  this.kilos = this.kilos / 10;
+}
 
-    
-     this.feetToMetre = this.feet / 3.28;
-     this.inchToMetre = this.inches * 0.0254;
-     this.feetToInch = this.feet * 12; 
-     this.meterToInch = this.meters * 39.370; 
-     this.inchToFeet = this.feet/12; 
-     this.metreToFeet = this.meters * 3.2808;
-     
-     if(this.pounds > this.stone && this.pounds > this.kilos){
-          this.kilos = this.poundToKilo;
-          this.stone = this.poundToStone;
-        if(this.feet > this.meters && this.feet > this.inches){
-          this.meters = this.feetToMetre;
-          this.inches = this.feetToInch;
+FeetAndInchesToMetre(){
+  this.value = 0;
+  this.value = this.inches + (this.feet *12);
+  this.meters = this.value * 0.0254;
+  this.meters = Math.round(this.meters * 100) / 100;
+}
+MetreToInchesAndFeet(){
+this.value = 0;
+this.value = this.meters / 0.0254;
+this.inches = this.value %12;
+this.feet = (this.value - this.inches) /12;
+this.inches = Math.round(this.inches);
+}
+BMI(){
+  this.bmi = (this.kilos / (this.meters * this.meters));
+  this.bmi = Math.round(this.bmi);
+ 
+ if(this.bmi < 18.5 ){
+   this.bmiString = "You are underweight";
+ }
+ else if(this.bmi >= 18.5 && this.bmi < 25){
+    this.bmiString = "You are of normal weight";
+ }
+ else if(this.bmi >= 25 && this.bmi < 30){
+    this.bmiString = "You are overweight";
+ }
+  else if( this.bmi >= 30){
+    this.bmiString = "You are heavily overweight";
+ }
 
-          this.bmi = this.kilos/(this.meters * this.meters);
-        }
-        else if(this.inches > this.meters && this.inches > this.feet){
-          this.meters = this.inchToMetre;
-          this.feet = this.inchToFeet;
+}
 
-          this.bmi = this.kilos/(this.meters * this.meters);
-        }
-        else if(this.meters > this.inches && this.meters > this.feet){
-          this.feet = this.metreToFeet;
-          this.inches = this.meterToInch;
-          
-          this.bmi = this.kilos/(this.meters * this.meters);
-        }
 
-     }
-     else if(this.stone > this.pounds && this.stone > this.kilos){
-       this.kilos = this.stoneToKilo;
-       this.pounds = this.stoneToPound;
-         if(this.feet > this.meters && this.feet > this.inches){
-             this.meters = this.feetToMetre;
-          this.inches = this.feetToInch;
 
-            this.bmi = this.kilos/(this.meters * this.meters);
-        }
-        else if(this.inches > this.meters && this.inches > this.feet){
-             this.meters = this.inchToMetre;
-             this.feet = this.inchToFeet;
 
-            this.bmi = this.kilos/(this.meters * this.meters);
-        }
-        else if(this.meters > this.inches && this.meters > this.feet){
-            this.feet = this.metreToFeet;
-            this.inches = this.meterToInch;
 
-            this.bmi = this.kilos/(this.meters * this.meters);
-        }
-     }
-     else if(this.kilos > this.pounds && this.kilos > this.stone){
-       this.pounds = this.kiloToPound;
-       this.stone = this.kiloToStone;
-         if(this.feet > this.meters && this.feet > this.inches){
-           this.meters = this.feetToMetre;
-           this.inches = this.feetToInch;
 
-           this.bmi = this.kilos/(this.meters * this.meters);
-        }
-        else if(this.inches > this.meters && this.inches > this.feet){
-             this.meters = this.inchToMetre;
-             this.feet = this.inchToFeet;
 
-            this.bmi = this.kilos/(this.meters * this.meters);
-        }
-        else if(this.meters > this.inches && this.meters > this.feet){
-             this.feet = this.metreToFeet;
-             this.inches = this.meterToInch;
 
-            this.bmi = this.kilos/(this.meters * this.meters); //finalBMICalc
-        }
-     }
-     
-     this.counter++;
-  }
+
+
 
   ngOnInit() {
   }
